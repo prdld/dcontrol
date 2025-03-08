3e2014ab244330364c3d27c48936bcb3916c0233
prdld <157811867+prdld@users.noreply.github.com>
2025-03-08 20:19:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export default {
-    aprUrl: "https://script.google.com/macros/s/AKfycbxVUinvWzsp178oiD7ldBJFlc3J-Bke1377PsJ9uuBIbrKgI-TlxuO0xtzmqSV3af0b/exec"
+    aprUrl: "https://script.google.com/macros/s/AKfycbz7LumCWTuael8VePvQRRY-Yh-1pcNXDIybwMYi8v4SyOLiIvO8SBWVKuVx1pe9HLRGYw/exec"
 }
